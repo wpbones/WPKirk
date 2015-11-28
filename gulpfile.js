91f2861334d199af33e47a6df059fba6336e677b
Giovambattista (John) Fazioli <john.fazioli@namecheap.com>
2015-11-28 15:10:05 +0100

    Minor fixes

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,10 @@
-var gulp = require( 'gulp' );
-var less = require( 'gulp-less' );
-var path = require( 'path' );
+var gulp = require( 'gulp' ),
+    less = require( 'gulp-less' ),
+    path = require( 'path' );
 
 gulp.task( 'less', function()
 {
-  return gulp.src( './resources/less/**/*.less' )
+  return gulp.src( './resources/assets/less/**/*.less' )
     .pipe( less( {
       paths : [ path.join( __dirname, 'less', 'includes' ) ]
     } ) )
