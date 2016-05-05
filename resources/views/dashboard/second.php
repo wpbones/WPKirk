f6df6b243cf9e3ef7bd50e87ac78d0d865cef4cc
Giovambattista (John) Fazioli <john.fazioli@namecheap.com>
2016-05-05 12:54:58 +0200

    Fixed docs

--- resources/views/dashboard/second.php
+++ resources/views/dashboard/second.php
@@ -1,6 +1,6 @@
 <!--
  |
- | In $this you'll find an instance of Plugin class.
+ | In $plugin you'll find an instance of Plugin class.
  | If you'd like can pass variable to this view, for example:
  |
  | return PluginClassName()->view( 'dashboard.index', [ 'var' => 'value' ] );
