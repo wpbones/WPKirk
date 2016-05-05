83e866c4ecfb57a6eb44c45206947b6bc03190eb
Giovambattista (John) Fazioli <john.fazioli@namecheap.com>
2016-05-05 12:54:41 +0200

    Added how display a custom config in a view

--- resources/views/dashboard/index.php
+++ resources/views/dashboard/index.php
@@ -1,6 +1,6 @@
 <!--
  |
- | In $this you'll find an instance of Plugin class.
+ | In $plugin you'll find an instance of Plugin class.
  | If you'd like can pass variable to this view, for example:
  |
  | return PluginClassName()->view( 'dashboard.index', [ 'var' => 'value' ] );
@@ -10,4 +10,9 @@
 <div class="kirk">
   <h1>Hello, I'm the first WPKirk view</h1>
   <p>You may get variable from the controller. For example, the variable <code>kirk</code>is <?php echo $kirk ?></p>
+
+  <h2>Configuration</h2>
+  <p>Get the <code>custom</code> configuration by using <code>echo $plugin->config( 'custom.sample' )</code></p>
+  <p><?php echo $plugin->config( 'custom.sample' ) ?></p>
+
 </div>
