6235cb91ebade30a2a894d0e33350dac619c3f89
gfazioli <giovambattista.fazioli@gmail.com>
2021-09-30 11:10:08 +0200

    adds miss api config

---
+++ config/api.php
@@ -0,0 +1,30 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| WordPress REST API configuration
+|--------------------------------------------------------------------------
+|
+| Here is where you can set up the WordPress REST API features.
+|
+*/
+
+return [
+
+    // embeded wp-json-server
+    'wp' => [
+        'require_authentication' => false, // will affect all routes.
+    ],
+
+    // your custom rest api
+    'custom' => [
+        'path' => '/api',
+        'enabled' => true,
+    ],
+
+    // authentication
+    'auth' => [
+        // embed basic authentication handler
+        'basic' => true
+    ]
+];
